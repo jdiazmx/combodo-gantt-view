f0ae2e4afb2b93481900be271acfe464f11ba812
Jorge Díaz <jorge.diaz@gmail.com>
2021-03-07 18:21:24 -0600

    Add spanish dictionary.

---
+++ es_cr.dict.combodo-gantt-view.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2020 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellaño', array(
+	// Dictionary entries go here
+	'GanttDashlet/Label' => 'Gantt',
+	'GanttDashlet/Description' => 'Ver objetos como un Gantt',
+	'GanttDashlet/NoPreviewAvailable' => 'No hay vista previa disponible!!',
+	'GanttDashlet/Prop:DefaultTitle' => 'Tickets',
+	'GanttDashlet/Prop:DefaultTitle2' => 'Contactos',
+
+	'GanttDashlet/Prop:Class' => 'Clase',
+	'GanttDashlet/Prop:name' => 'Nombre',
+	'GanttDashlet/Prop:StartDate' => 'Fecha de inicio~~',
+	'GanttDashlet/Prop:EndDate' => 'Fecha final~~',
+	'GanttDashlet/Prop:Title' => 'Título',
+	'GanttDashlet/Prop:DependsOn' => 'Depende de',
+	'GanttDashlet/Prop:ColoredField' => 'Coloreado por',
+	'GanttDashlet/Prop:AdditionalInfoLeft' => 'Información adicional 1',
+	'GanttDashlet/Prop:AdditionalInfoRight' => 'Información adicional 2',
+	'GanttDashlet/Attribute:ActionDisabled' => 'Acción inhabilitada para este objeto',
+	'GanttDashlet:UI/Tab:GanttViewer' => "Vista de Gantt",
+	'GanttDashlet/Prop:ParentField' => 'Agrupar por',
+	'GanttDashlet/Prop:Percentage' => 'Porcentaje de avance',
+	'GanttDashlet/Prop:GroupByInformations' => 'Agrupar por informaciones',
+));
